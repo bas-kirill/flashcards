8f086d021846fc83b7d7ca0851f55ded9eb3553e
eertree <baskirill.an@gmail.com>
2022-11-23 22:11:39 +0200

    flashcards: init

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"bufio"
+	"os"
+	"strings"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	line, _ := reader.ReadString('\n')
+	line = strings.TrimSpace(line)
+	// your code ...
+}
