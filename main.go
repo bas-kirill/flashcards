a84887f8a6943be4da247d8209148bcc07f362a0
eertree <baskirill.an@gmail.com>
2022-11-23 22:59:25 +0200

    flashcards: init

--- main.go
+++ main.go
@@ -2,13 +2,25 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"os"
 	"strings"
 )
 
-func main() {
-	reader := bufio.NewReader(os.Stdin)
+func readUserInput(reader *bufio.Reader) string {
 	line, _ := reader.ReadString('\n')
 	line = strings.TrimSpace(line)
-	// your code ...
+	return line[2:]
+}
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	_ = readUserInput(reader)
+	definition := readUserInput(reader)
+	answer := readUserInput(reader)
+	if definition == answer {
+		fmt.Println("right")
+	} else {
+		fmt.Println("wrong")
+	}
 }
