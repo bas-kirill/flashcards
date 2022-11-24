729df7f230254bdbb167a9dd0adf8f8830dbcafd
eertree <eertree@zero10.app>
2022-11-24 11:38:27 +0200

    flashcards: add cards number

--- main.go
+++ main.go
@@ -4,23 +4,64 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
 
 func readUserInput(reader *bufio.Reader) string {
 	line, _ := reader.ReadString('\n')
 	line = strings.TrimSpace(line)
-	return line[2:]
+	//fmt.Println(line)
+	return line
+}
+
+func readCardsNumber(reader *bufio.Reader) (int, error) {
+	fmt.Println("Input the number of cards:")
+	inp := readUserInput(reader)
+	cards, err := strconv.ParseInt(inp, 10, 32)
+	if err != nil {
+		return -1, err
+	}
+	return int(cards), nil
+}
+
+func readTerm(reader *bufio.Reader, idx int) string {
+	fmt.Printf("The term for card #%d:\n", idx)
+	term := readUserInput(reader)
+	return term
+}
+
+func readDefinition(reader *bufio.Reader, idx int) string {
+	fmt.Printf("The definition for card #%d:\n", idx)
+	definition := readUserInput(reader)
+	return definition
+}
+
+func readAnswer(reader *bufio.Reader, term string) string {
+	fmt.Printf("Print the definition of \"%s\":\n", term)
+	ans := readUserInput(reader)
+	return ans
 }
 
 func main() {
 	reader := bufio.NewReader(os.Stdin)
-	_ = readUserInput(reader)
-	definition := readUserInput(reader)
-	answer := readUserInput(reader)
-	if definition == answer {
-		fmt.Println("right")
-	} else {
-		fmt.Println("wrong")
+	cards, _ := readCardsNumber(reader)
+	terms := []string{}
+	definitions := []string{}
+	for i := 1; i <= cards; i++ {
+		term := readTerm(reader, i)
+		definition := readDefinition(reader, i)
+		terms = append(terms, term)
+		definitions = append(definitions, definition)
+	}
+
+	for i := 0; i < cards; i++ {
+		term, def := terms[i], definitions[i]
+		ans := readAnswer(reader, term)
+		if ans == definitions[i] {
+			fmt.Println("Correct!")
+		} else {
+			fmt.Printf("Wrong. The right answer is \"%s\".\n", def)
+		}
 	}
 }
