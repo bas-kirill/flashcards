3afe2782f40b2169431fa18dcf562574c0cebbee
eertree <eertree@zero10.app>
2022-11-24 14:24:17 +0200

    flashcards: unique terms

--- main.go
+++ main.go
@@ -11,7 +11,6 @@ import (
 func readUserInput(reader *bufio.Reader) string {
 	line, _ := reader.ReadString('\n')
 	line = strings.TrimSpace(line)
-	//fmt.Println(line)
 	return line
 }
 
@@ -25,15 +24,32 @@ func readCardsNumber(reader *bufio.Reader) (int, error) {
 	return int(cards), nil
 }
 
-func readTerm(reader *bufio.Reader, idx int) string {
+func contains(array []string, word string) bool {
+	for _, elem := range array {
+		if elem == word {
+			return true
+		}
+	}
+	return false
+}
+
+func readTerm(reader *bufio.Reader, terms []string, idx int) string {
 	fmt.Printf("The term for card #%d:\n", idx)
 	term := readUserInput(reader)
+	for contains(terms, term) {
+		fmt.Printf("The term \"%s\" already exists. Try again:\n", term)
+		term = readUserInput(reader)
+	}
 	return term
 }
 
-func readDefinition(reader *bufio.Reader, idx int) string {
+func readDefinition(reader *bufio.Reader, definitions []string, idx int) string {
 	fmt.Printf("The definition for card #%d:\n", idx)
 	definition := readUserInput(reader)
+	for contains(definitions, definition) {
+		fmt.Printf("The definition \"%s\" already exists. Try again:\n", definition)
+		definition = readUserInput(reader)
+	}
 	return definition
 }
 
@@ -43,25 +59,43 @@ func readAnswer(reader *bufio.Reader, term string) string {
 	return ans
 }
 
+func appliedDefToAnotherTerm(
+	terms []string,
+	definitions []string,
+	userDef string,
+) (bool, string) {
+	for i, elem := range definitions {
+		if userDef == elem {
+			return true, terms[i]
+		}
+	}
+	return false, ""
+}
+
 func main() {
 	reader := bufio.NewReader(os.Stdin)
 	cards, _ := readCardsNumber(reader)
 	terms := []string{}
 	definitions := []string{}
 	for i := 1; i <= cards; i++ {
-		term := readTerm(reader, i)
-		definition := readDefinition(reader, i)
+		term := readTerm(reader, terms, i)
+		definition := readDefinition(reader, definitions, i)
 		terms = append(terms, term)
 		definitions = append(definitions, definition)
 	}
 
 	for i := 0; i < cards; i++ {
 		term, def := terms[i], definitions[i]
-		ans := readAnswer(reader, term)
-		if ans == definitions[i] {
+		userDef := readAnswer(reader, term)
+		if userDef == def {
 			fmt.Println("Correct!")
 		} else {
-			fmt.Printf("Wrong. The right answer is \"%s\".\n", def)
+			ok, anotherTerm := appliedDefToAnotherTerm(terms, definitions, userDef)
+			if ok {
+				fmt.Printf("Wrong. The right answer is \"%s\", but your definition is correct for \"%s\".\n", def, anotherTerm)
+			} else {
+				fmt.Printf("Wrong. The right answer is \"%s\".\n", def)
+			}
 		}
 	}
 }
