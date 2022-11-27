9eda2212290292a31e6f498d901eb6cead34579b
eertree <baskirill.an@gmail.com>
2022-11-27 18:59:07 +0200

    flashcards: add, remove, import, export, ask, exit, log, hardest card

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -164,6 +165,14 @@ func (l *List[T]) Front() *Element[T] {
 	return l.root.next
 }
 
+// Back returns the last element of list l or nil if the list is empty.
+func (l *List[T]) Back() *Element[T] {
+	if l.len == 0 {
+		return nil
+	}
+	return l.root.prev
+}
+
 // Oldest returns a pointer to the oldest pair. It's meant to be used to iterate on the ordered map's
 // pairs from the oldest to the newest, e.g.:
 // for pair := orderedMap.Oldest(); pair != nil; pair = pair.Next() { fmt.Printf("%v => %v\n", pair.Key, pair.Value) }
@@ -184,64 +193,77 @@ func (p *Pair[K, V]) Next() *Pair[K, V] {
 	return listElementToPair(p.element.Next())
 }
 
+type TermError struct {
+	Term   string
+	Errors int
+}
+
 type Cards struct {
 	TermToDef *OrderedMap[string, string]
-	DefToTerm *OrderedMap[string, string]
+	DefToTerm *OrderedMap[string, TermError]
 }
 
 func NewCards() *Cards {
 	return &Cards{
 		TermToDef: New[string, string](),
-		DefToTerm: New[string, string](),
+		DefToTerm: New[string, TermError](),
 	}
 }
 
 type Card struct {
 	Term       string `json:"term"`
 	Definition string `json:"def"`
+	ErrorCount int    `json:"errors"`
 }
 
-func readUserInput(reader *bufio.Reader) string {
+var logger *List[string]
+
+func ReadUserInput(reader *bufio.Reader) string {
 	line, _ := reader.ReadString('\n')
 	line = strings.TrimSpace(line)
 	line = strings.TrimSuffix(line, "\n")
 	return line
 }
 
-func tryAddCardTerm(cards *Cards, term string) bool {
+func TryAddCardTerm(cards *Cards, term string) bool {
 	_, termPresent := cards.TermToDef.Get(term)
 	if !termPresent {
 		return true
 	} else {
 		fmt.Printf("The card \"%s\" already exists. Try again:\n", term)
+		logger.PushBack(fmt.Sprintf("The card \"%s\" already exists. Try again:\n", term))
 		return false
 	}
 }
 
-func tryAddCardDef(cards *Cards, def string) bool {
+func TryAddCardDef(cards *Cards, def string) bool {
 	_, defPresent := cards.DefToTerm.Get(def)
 	if !defPresent {
 		return true
 	} else {
 		fmt.Printf("The definition \"%s\" already exists. Try again:\n", def)
+		//cards.DefToTerm.Set(def, TermError{termErr.Term, termErr.Errors + 1})
+		logger.PushBack(fmt.Sprintf("The definition \"%s\" already exists. Try again:\n", def))
 		return false
 	}
 }
 
-func removeCard(cards *Cards, term string) bool {
+func RemoveCard(cards *Cards, term string) bool {
 	def, ok := cards.TermToDef.Get(term)
 	if ok {
 		cards.DefToTerm.Delete(def)
 		cards.TermToDef.Delete(term)
 		fmt.Println("The card has been removed.")
+		logger.PushBack("The card has been removed.")
 		return true
 	} else {
 		fmt.Printf("Can't remove \"%s\": there is no such card.\n", term)
+		logger.PushBack(fmt.Sprintf("Can't remove \"%s\": there is no such card.\n", term))
 		return false
 	}
 }
 
-func importCards(file *os.File, cards *Cards) int {
+func ImportCards(file *os.File, cards *Cards) int {
 	imported := 0
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
@@ -252,28 +274,29 @@ func importCards(file *os.File, cards *Cards) int {
 			log.Fatal(err)
 		}
 		cards.TermToDef.Set(card.Term, card.Definition)
-		cards.DefToTerm.Set(card.Definition, card.Term)
+		//fmt.Println(card.Term, card.Definition, card.ErrorCount)
+		cards.DefToTerm.Set(card.Definition, TermError{card.Term, card.ErrorCount})
 		imported++
 	}
 	return imported
 }
 
-func exportCards(file *os.File, cards *Cards) int {
+func ExportCards(file *os.File, cards *Cards) int {
+	defer file.Close()
 	exported := 0
 	writer := bufio.NewWriter(file)
 	for pair := cards.TermToDef.Oldest(); pair != nil; pair = pair.Next() {
 		term, def := pair.Key, pair.Value
-		card := Card{Term: term, Definition: def}
+		errors, _ := cards.DefToTerm.Get(def)
+		card := Card{Term: term, Definition: def, ErrorCount: errors.Errors}
 		cardJSON, err := json.Marshal(card)
 		if err != nil {
 			log.Fatal(err)
 		}
-		//_, _ = writer.WriteString(string(cardJSON))
 		_, err = fmt.Fprintln(writer, string(cardJSON))
 		if err != nil {
 			log.Fatal(err)
 		}
-		//_ = writer.Flush()
 		err = writer.Flush()
 		if err != nil {
 			log.Fatal(err)
@@ -283,8 +306,9 @@ func exportCards(file *os.File, cards *Cards) int {
 	return exported
 }
 
-func readAsks() int {
+func ReadAsks() int {
 	fmt.Println("How many times to ask?")
+	logger.PushBack("How many times to ask?")
 	var asks int
 	_, err := fmt.Scan(&asks)
 	if err != nil {
@@ -293,7 +317,7 @@ func readAsks() int {
 	return asks
 }
 
-func appliedDefToAnotherTerm(cards *Cards, userDef string) (bool, string) {
+func ApplyDefToAnotherTerm(cards *Cards, userDef string) (bool, string) {
 	for pair := cards.TermToDef.Oldest(); pair != nil; pair = pair.Next() {
 		term, def := pair.Key, pair.Value
 		if userDef == def {
@@ -303,61 +327,135 @@ func appliedDefToAnotherTerm(cards *Cards, userDef string) (bool, string) {
 	return false, ""
 }
 
+func SaveLog(file *os.File) {
+	fmt.Println("kek")
+	writer := bufio.NewWriter(file)
+	for elem := logger.Front(); elem != logger.Back().next; elem = elem.next {
+		fmt.Println(elem.Value)
+		_, err := fmt.Fprintln(writer, elem.Value)
+		if err != nil {
+			log.Fatal(err)
+		}
+		err = writer.Flush()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+}
+
+func HardestCard(cards *Cards) string {
+	term := ""
+	mxErr := -1
+	var terms []string
+	for pair := cards.DefToTerm.Oldest(); pair != nil; pair = pair.Next() {
+		termError := pair.Value
+		if termError.Errors > mxErr {
+			mxErr = termError.Errors
+			term = termError.Term
+			terms = []string{term}
+		} else if termError.Errors == mxErr {
+			terms = append(terms, term)
+		}
+	}
+
+	if mxErr == 0 || cards.DefToTerm.list.len == 0 {
+		return "There are no cards with errors."
+	} else if len(terms) == 1 {
+		return fmt.Sprintf("The hardest card is \"%s\". You have %d errors answering it", term, mxErr)
+	} else if len(terms) > 1 {
+		ans := ""
+		first := true
+		for t := range terms {
+			if !first {
+				ans += ", "
+			}
+			first = false
+			ans += fmt.Sprintf("\"%s\"", t)
+		}
+		return fmt.Sprintf("The hardest cards are %s", ans)
+	}
+	return "-1"
+}
+
 func main() {
+	logger = NewList[string]()
 	reader := bufio.NewReader(os.Stdin)
 	cards := NewCards()
 	cmd := ""
 	for cmd != "exit" {
-		fmt.Println("Input the action (add, remove, import, export, ask, exit):")
-		cmd = readUserInput(reader)
+		fmt.Println("Input the action (add, remove, import, export, ask, exit, log, hardest card, reset stats):")
+		logger.PushBack("Input the action (add, remove, import, export, ask, exit, log, hardest card, reset stats):")
+
+		cmd = ReadUserInput(reader)
+		logger.PushBack(cmd)
 
 		switch cmd {
 		case "add":
 			fmt.Println("The card:")
-			term := readUserInput(reader)
-			termPresent := tryAddCardTerm(cards, term)
+			logger.PushBack("The card:")
+
+			term := ReadUserInput(reader)
+			logger.PushBack(term)
+
+			termPresent := TryAddCardTerm(cards, term)
 			for !termPresent {
-				term = readUserInput(reader)
-				termPresent = tryAddCardTerm(cards, term)
+				term = ReadUserInput(reader)
+				logger.PushBack(term)
+				termPresent = TryAddCardTerm(cards, term)
 			}
 
 			fmt.Println("The definition of the card:")
-			def := readUserInput(reader)
-			defPresent := tryAddCardDef(cards, def)
+			logger.PushBack("The definition of the card:")
+
+			def := ReadUserInput(reader)
+			logger.PushBack(def)
+			defPresent := TryAddCardDef(cards, def)
 			for !defPresent {
-				def = readUserInput(reader)
-				defPresent = tryAddCardDef(cards, def)
+				def = ReadUserInput(reader)
+				logger.PushBack(def)
+				defPresent = TryAddCardDef(cards, def)
 			}
 
 			cards.TermToDef.Set(term, def)
-			cards.DefToTerm.Set(def, term)
+			cards.DefToTerm.Set(def, TermError{term, 0})
 
 			fmt.Printf("The pair (\"%s\":\"%s\") has been added.\n", term, def)
+			logger.PushBack(fmt.Sprintf("The pair (\"%s\":\"%s\") has been added.", term, def))
 		case "remove":
 			fmt.Println("Which card?")
-			term := readUserInput(reader)
-			removeCard(cards, term)
+			logger.PushBack("Which card?")
+			term := ReadUserInput(reader)
+			logger.PushBack(term)
+			RemoveCard(cards, term)
 		case "import":
 			fmt.Println("File name:")
-			fileName := readUserInput(reader)
+			logger.PushBack("File name:")
+			fileName := ReadUserInput(reader)
+			logger.PushBack(fileName)
 			file, err := os.OpenFile(fileName, os.O_RDONLY, 0444)
 			if err != nil {
 				fmt.Println("File not found.")
+				logger.PushBack("File not found.")
 				break
 			}
-			loadedCards := importCards(file, cards)
+			loadedCards := ImportCards(file, cards)
 			fmt.Printf("%d cards have been loaded.\n", loadedCards)
+			logger.PushBack(fmt.Sprintf("%d cards have been loaded.", loadedCards))
 		case "export":
 			fmt.Println("File name:")
-			fileName := readUserInput(reader)
+			logger.PushBack("File name:")
+			fileName := ReadUserInput(reader)
+			logger.PushBack(fileName)
 			file, err := os.OpenFile(fileName, os.O_CREATE|os.O_WRONLY, 0644)
 			if err != nil {
 				log.Fatal(err)
 			}
-			exportedCards := exportCards(file, cards)
+			exportedCards := ExportCards(file, cards)
 			fmt.Printf("%d cards have been saved.\n", exportedCards)
+			logger.PushBack(fmt.Sprintf("%d cards have been saved.", exportedCards))
 		case "ask":
-			asks := readAsks()
+			asks := ReadAsks()
+			logger.PushBack(strconv.FormatInt(int64(asks), 10))
 			idx := 0
 			for pair := cards.TermToDef.Oldest(); idx < asks; pair, idx = pair.Next(), idx+1 {
 				if pair == nil {
@@ -365,23 +463,56 @@ func main() {
 				}
 				term, def := pair.Key, pair.Value
 				fmt.Printf("Print the definition of \"%s\":\n", term)
-				userDef := readUserInput(reader)
+				logger.PushBack(fmt.Sprintf("Print the definition of \"%s\":", term))
+
+				userDef := ReadUserInput(reader)
+				logger.PushBack(userDef)
+
 				if userDef == def {
 					fmt.Println("Correct!")
+					logger.PushBack("Correct!")
 				} else {
-					ok, anotherTerm := appliedDefToAnotherTerm(cards, userDef)
+					ok, anotherTerm := ApplyDefToAnotherTerm(cards, userDef)
 					if ok {
 						fmt.Printf("Wrong. The right answer is \"%s\", but your definition is correct for \"%s\".\n", def, anotherTerm)
+						logger.PushBack(fmt.Sprintf("Wrong. The right answer is \"%s\", but your definition is correct for \"%s\".", def, anotherTerm))
 					} else {
 						fmt.Printf("Wrong. The right answer is \"%s\".\n", def)
+						logger.PushBack(fmt.Sprintf("Wrong. The right answer is \"%s\".", def))
 					}
+					termErr, _ := cards.DefToTerm.Get(def)
+					cards.DefToTerm.Set(def, TermError{termErr.Term, termErr.Errors + 1})
 				}
 			}
 		case "exit":
 			fmt.Print("Bye bye!")
+			logger.PushBack("Bye bye!")
 			os.Exit(0)
+		case "log":
+			fmt.Println("File name:")
+			logger.PushBack("File name:")
+			fileName := ReadUserInput(reader)
+			logger.PushBack(fileName)
+			file, err := os.OpenFile(fileName, os.O_CREATE|os.O_WRONLY, 0644)
+			if err != nil {
+				log.Fatal(err)
+			}
+			fmt.Println("The log has been saved.")
+			logger.PushBack("The log has been saved.")
+			SaveLog(file)
+		case "hardest card":
+			ans := HardestCard(cards)
+			fmt.Println(ans)
+			logger.PushBack(ans)
+		case "reset stats":
+			for pair := cards.DefToTerm.Oldest(); pair != nil; pair = pair.Next() {
+				cards.DefToTerm.Set(pair.Key, TermError{Term: pair.Value.Term, Errors: 0})
+			}
+			fmt.Println("Card statistics have been reset.")
+			logger.PushBack("Card statistics have been reset.")
 		}
 
 		fmt.Println()
+		logger.PushBack("")
 	}
 }
